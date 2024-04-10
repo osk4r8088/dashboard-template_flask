8c9cddc3d75574e0dcc0ce36b554f9dd7df79c86
Oskar <oskar.schulz@gmail.com>
2024-04-10 14:57:45 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#test route
+@app.route('/')
+#def index():
+#    return "<h1>This shit works 1</h1>"
+
+def index():
+    first_name = "John"
+    last_name = "Doe"
+    return render_template("index.html", first_name=first_name, last_name=last_name)
+
+#localhost user
+@app.route('/user/<name>')
+
+def user(name):
+    return render_template("user.html", username=name)
+
+
+#404 page not found
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+#Internal server error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
